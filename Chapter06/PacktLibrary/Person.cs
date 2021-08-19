aaaf5d8f45421f963d6b64f4dbc11c28b89babe1
eyub <eyub2001@gmail.com>
2021-08-19 23:36:34 +0300

    chapter 6 - simplifying methods

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -16,5 +16,44 @@ namespace Packt.Shared
         {
             WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
         }
+
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person()
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        public static Person operator *(Person p1, Person p2)
+        {
+            return Procreate(p1, p2);
+        }
+
+        public static int Factorial(int number)
+        {
+            if (number < 0)
+            {
+                throw new ArgumentException($"{nameof(number)} cannot be less than zero.");
+            }
+
+            return localFactorial(number);
+
+            int localFactorial(int localNumber)
+            {
+                if (localNumber < 1) return 1;
+                return localNumber * localFactorial(localNumber - 1);
+            }
+        }
     }
 }
