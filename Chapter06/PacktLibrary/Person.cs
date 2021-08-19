974aa83ac47460d8085284221570e281f38f4409
Eyub Yildirim <yildirimey19@itu.edu.tr>
2021-08-19 22:02:28 +0300

    chapter 6 - setting up a class library and console application

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
+        public List<Person> Children = new List<Person>();
+        
+        // methods
+        public void WriteToConsole()
+        {
+            WriteLine($"{Name} was born on a {DateOfBirth:dddd}");
+        }
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            
+        }
+    }
+}
