1f32a148f2dc8daf09f47248372b6b0a29e0684f
eyubyildirim <eyub2001@gmail.com>
2021-09-30 21:59:43 +0300

    chapter 06 - inheriting from classes

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -71,5 +71,11 @@ namespace Packt.Shared
         {
             return Name.CompareTo(other.Name);
         }
+
+        public override string ToString()
+        {
+            return $"{Name} is a {base.ToString()}";
+        }
+
     }
 }
