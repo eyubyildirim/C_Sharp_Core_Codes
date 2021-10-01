cc6bd4e9ddbd0c3e35b670f60f1a55c9d96ae333
eyubyildirim <eyub2001@gmail.com>
2021-10-01 22:54:47 +0300

    chapter 07 - understanding .net core components

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Linq;
+using static System.Console;
+
+namespace AssembliesAndNamespaces
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var doc = new XDocument();
+
+            string s1 = "Hello";
+            String s2 = "World";
+        }
+    }
+}
