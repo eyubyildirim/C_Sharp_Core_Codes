492049c7a04b4d509b4bee9b84b0651e05a7f08f
eyubyildirim <eyub2001@gmail.com>
2021-10-01 23:07:37 +0300

    chapter 07 - publishing your applications for deployment

---
+++ Chapter07/DotNetCoreEverywhere/Program.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace DotNetCoreEverywhere
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            WriteLine("I can run everywhere!");
+        }
+    }
+}
